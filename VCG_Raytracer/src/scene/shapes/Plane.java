20bcbae5f4d1b70691b3a90eaf79cb8548b3fc3b
Alexander Wollert <alexander@4-wollert.de>
2016-02-17 14:44:27 +0100

    Fix Plane intersection (not complete, yet)

--- VCG_Raytracer/src/scene/shapes/Plane.java
+++ VCG_Raytracer/src/scene/shapes/Plane.java
@@ -18,19 +18,25 @@ public class Plane extends Shape {
     public static int FACING_FRONT = 4;
 
     public Plane(Vec3 pos, Material mat, int facingDirection) {
-        super(pos, mat);
+        super(pos, mat, "PLANE");
 
         // The normal of a plane is always the vector coming from the position showing to the center of the scene
         mNormal = getFacingNormal(facingDirection);
+    }
+
+    public Plane(Vec3 pos, Material mat, Vec3 normal) {
+        super(pos, mat, "PLANE");
+
+        mNormal = normal;
     }
 
     public Vec3 getFacingNormal(int facingDirection){
         switch( facingDirection ){
             case 0 : return new Vec3( 1, 0, 0);
             case 1 : return new Vec3( -1, 0, 0);
-            case 3 : return new Vec3( 0, -1, 0);
-            case 4 : return new Vec3( 0, 1, 0);
-            case 5 : return new Vec3( 0, 0, -1);
+            case 2 : return new Vec3( 0, -1, 0);
+            case 3 : return new Vec3( 0, 1, 0);
+            case 4 : return new Vec3( 0, 0, -1);
             default:
                 Log.warn(this, "Plane cannot be created. The facing direction is unclear.");
             return new Vec3( 0, 0 ,0 );
@@ -44,26 +50,34 @@ public class Plane extends Shape {
         // Pn * D from slides
         float intersectionTestValue = mNormal.scalar(ray.getDirection());
 
-        if( intersectionTestValue >= 0 ){
+        if( intersectionTestValue < 0 ){
             return intersectionTest;
-        } else {
+        } else if( intersectionTestValue > 0 ){
+            intersectionTest.setIncoming( false );
+            return fillIntersectionInfo(intersectionTest, ray, intersectionTestValue);
+        }
+        else{
+            intersectionTest.setIncoming( true );
             return fillIntersectionInfo(intersectionTest, ray, intersectionTestValue);
         }
     }
 
     private Intersection fillIntersectionInfo(Intersection intersectionTest, Ray ray, float intersectionTestValue){
 
+        intersectionTest.setHit(true);
+
+        float distanceToOrigin = getPosition().sub(ray.getStartPoint()).scalar(mNormal);
+
         // Q from slides
-        float distanceToOrigin = getPosition().length();
+        float t = ( distanceToOrigin / intersectionTestValue );
 
-        // Pn * P0 from slides
-        float normalPositionValue = getPosition().scalar(mNormal);
-
-        float t = -(( normalPositionValue + distanceToOrigin ) / intersectionTestValue );
-
-        Vec3 intersectionPoint = ray.getDirection().multScalar(t);
+        //Vec3 intersectionPoint = ray.getStartPoint().add( ray.getDirection().multScalar(intersectionTestValue) );
+        Vec3 intersectionPoint = ray.getStartPoint().add( ray.getDirection().multScalar(t) );
 
         intersectionTest.setIntersectionPoint(intersectionPoint);
+        intersectionTest.setNormal(mNormal);
+
+        //Log.error(this, intersectionPoint.toString());
 
         return intersectionTest;
     }
