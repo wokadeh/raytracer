091370226494fb1862566444f289dfc7b7e8823b
Alexander Wollert <alexander@4-wollert.de>
2016-03-15 09:44:15 +0100

    Add AreaLight for Soft Shadows

---
+++ VCG_Raytracer/src/scene/lights/AreaLight.java
@@ -0,0 +1,11 @@
+package scene.lights;
+
+import utils.RgbColor;
+import utils.Vec3;
+
+public class AreaLight  extends Light {
+
+	public AreaLight(Vec3 pos, RgbColor color) {
+		super(pos, color);
+	}
+}
