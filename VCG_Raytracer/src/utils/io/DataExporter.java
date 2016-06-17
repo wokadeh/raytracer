845838cd8f2f4e4ed79e6ef2b70356c3806c2408
Alexander Wollert <alexander@4-wollert.de>
2016-06-17 18:38:14 +0200

    Renaming importers and exporters

--- VCG_Raytracer/src/utils/io/DataExporter.java
+++ VCG_Raytracer/src/utils/io/DataExporter.java
@@ -5,13 +5,13 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
-public class ImageExporter {
-    public static void saveImageToPng(BufferedImage image, String fileName){
+public class DataExporter {
+    public static void exportImageToPng(BufferedImage image, String fileName){
         File outFile = new File(fileName);
         try {
             ImageIO.write(image, "png", outFile);
         } catch (Exception e) {
-            System.err.println(e.getMessage()); // print any ImageExporter errors to stderr.
+            System.err.println(e.getMessage()); // print any DataExporter errors to stderr.
         }
     }
 }
