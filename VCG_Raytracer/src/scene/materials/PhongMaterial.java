d7274908a2e677fb40af2c2b31e17c9d69edc363
Alexander Wollert <alexander@4-wollert.de>
2016-07-10 17:45:45 +0200

    Fix Zero shininess (is 1 not 0)

--- VCG_Raytracer/src/scene/materials/PhongMaterial.java
+++ VCG_Raytracer/src/scene/materials/PhongMaterial.java
@@ -8,7 +8,7 @@ public class PhongMaterial extends Material {
 
     public static float SHINY = 10;
     public static float VERY_SHINY = 100;
-    public static float NOT_SHINY = 0;
+    public static float NOT_SHINY = 1;
 
     public PhongMaterial(RgbColor ambientCoefficient, RgbColor diffuseCoefficient, RgbColor specularCoefficient, float shininess, float reflectivity, float fractionCoeff, float transparency) {
         super(ambientCoefficient, diffuseCoefficient, specularCoefficient, shininess, "PHONG");
