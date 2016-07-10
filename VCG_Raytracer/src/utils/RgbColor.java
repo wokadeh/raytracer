2950cdbb8aa1c7eccfb1008d362ba95214e50267
Alexander Wollert <alexander@4-wollert.de>
2016-07-10 17:43:38 +0200

    Bugfix & customization of custom colors

--- VCG_Raytracer/src/utils/RgbColor.java
+++ VCG_Raytracer/src/utils/RgbColor.java
@@ -9,13 +9,13 @@ public class RgbColor {
     public float green(){ return colors.y; }
     public float blue(){ return colors.z; }
 
-    public static RgbColor DARK_CUSTOM = new RgbColor(0.08f, 0.01f, 0.01f);
+    public static RgbColor DARK_CUSTOM = new RgbColor(0.02f, 0.01f, 0.01f);
     public static RgbColor RED = new RgbColor(1, 0, 0);
-    public static RgbColor DARK_RED = new RgbColor(.1f, 0, 0);
+    public static RgbColor DARK_RED = new RgbColor(.01f, 0, 0);
     public static RgbColor GREEN = new RgbColor(0, 1, 0);
-    public static RgbColor DARK_GREEN = new RgbColor(0, .1f, 0);
+    public static RgbColor DARK_GREEN = new RgbColor(0, .01f, 0);
     public static RgbColor BLUE = new RgbColor(0, 0, 1);
-    public static RgbColor DARK_BLUE = new RgbColor(0, 0, .1f);
+    public static RgbColor DARK_BLUE = new RgbColor(0, 0, .01f);
     public static RgbColor WHITE = new RgbColor(1, 1, 1);
     public static RgbColor BLACK = new RgbColor(0, 0, 0);
     public static RgbColor CYAN = new RgbColor(0, 1, 1);
@@ -24,7 +24,7 @@ public class RgbColor {
     public static RgbColor GRAY = new RgbColor(0.5f, 0.5f, 0.5f);
     public static RgbColor SOFT_GRAY = new RgbColor(0.20f, 0.20f, 0.20f);
     public static RgbColor LIGHT_GRAY = new RgbColor(0.25f, 0.25f, 0.25f);
-    public static RgbColor DARK_GRAY = new RgbColor(0.01f, 0.01f, 0.01f);
+    public static RgbColor DARK_GRAY = new RgbColor(0.1f, 0.1f, 0.1f);
 
     public RgbColor(float r, float g, float b){
         colors = new Vec3(r, g, b);
@@ -41,7 +41,7 @@ public class RgbColor {
     public void add(float r, float g, float b){
         colors.x += r;
         colors.y += g;
-        colors.z += g;
+        colors.z += b;
 
         clamp();
     }
