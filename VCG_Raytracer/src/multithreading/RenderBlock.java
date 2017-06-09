334f2627812445dd0427b2d1865ddbc7e6f01089
Alexander <alexander.wollert@hs-duesseldorf.de>
2017-06-09 12:34:42 +0200

    Introduce Renderblock

---
+++ VCG_Raytracer/src/multithreading/RenderBlock.java
@@ -0,0 +1,15 @@
+package multithreading;
+
+public class RenderBlock{
+	public int xMin;
+	public int yMin;
+	public int xMax;
+	public int yMax;
+
+	RenderBlock( int xMin, int yMin, int xMax, int yMax){
+		xMin = xMin;
+		yMin = yMin;
+		xMax = xMax;
+		yMax = yMax;
+	}
+}
